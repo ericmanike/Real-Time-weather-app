baeedc8eccf91a518fc65548609b7304ce478cf6
Eric Manike Haare <manikeeric@gmail.com>
2025-12-28 10:09:51 +0000

    Adjust layout height and icon size in Layout component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,9 +4,9 @@ import { FaCloudMoon } from 'react-icons/fa6'
 
 function Layout() {
   return (<>
-    <div className='w-full h-[150px] bg-[#071d44] md:text-2xl text-white flex flex-row flex-nowrap justify-between items-center gap-20  px-5 md:px-15 '>
+    <div className='w-full h-[120px] bg-[#071d44] text-[14px] md:text-1xl text-white flex flex-row flex-nowrap justify-between items-center gap-20  px-5 md:px-15 '>
 
-        <FaCloudMoon className='text-[gold] h-20 w-20' />
+        <FaCloudMoon className='text-[gold] h-16 w-16' />
         <div className='flex flex-row justify-center items-center gap-10  w-fit '>
     <span 
     className="relative after:content-[''] after:absolute  after:bottom-0 after:left-0 after:w-0 after:h-1 hover:after:w-full after:bg-[maroon] after:transition-all after:duration-500  after:ease-in-out"
